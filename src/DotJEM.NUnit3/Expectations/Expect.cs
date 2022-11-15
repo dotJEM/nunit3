21396a189fa89f7c97207b309e9c35b2eb8a8e67
Jens Melgaard <admin@it-links.dk>
2022-11-15 23:50:53 +0100

    Playing with a more extendable pattern for Assertions.

---
+++ src/DotJEM.NUnit3/Expectations/Expect.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotJEM.NUnit3.Expectations
+{
+    public class Assert : NUnit.Framework.Assert
+    {
+        public static void That(params ExpectationBuilder[] expectation)
+        {
+
+        }
+    }
+
+    public class Expect
+    {
+        public static ExpectationBuilder<T> That<T>(T value)
+        {
+            return null;
+        }
+
+
+    }
+
+
+    public static class StaticImport
+    {
+        public static ExpectationBuilder<T> Expect<T>(T value)
+        {
+            return new ExpectationBuilder<T>(value);
+        }
+    }
+    public interface IExpectation
+    {
+
+    }
+
+    public abstract class Expectation : IExpectation
+    {
+
+    }
+
+    public class AndExpectationBuilder : ExpectationBuilder
+    {
+        private readonly ExpectationBuilder[] expectations;
+
+        public AndExpectationBuilder(params ExpectationBuilder[] expectations)
+        {
+            this.expectations = expectations;
+        }
+    }
+
+    public class ExpectationBuilder
+    {
+        public static AndExpectationBuilder operator &(ExpectationBuilder left, ExpectationBuilder right)
+        {
+            return new AndExpectationBuilder(left, right);
+        }
+    }
+
+    public class ExpectationBuilder<T>: ExpectationBuilder
+    {
+        public ExpectationBuilder(T value)
+        {
+        }
+    }
+
+    public static class ObjectExtentions
+    {
+        public static ExpectationBuilder<T> Is<T>(this T value)
+        {
+            return new ExpectationBuilder<T>(value);
+        }
+    }
+
+    public static class ExpectationBuilderExtensions {
+        public static ExpectationBuilder<T> EqualTo<T>(this ExpectationBuilder<T> self, T other)
+        {
+            return self.Capture();
+        }
+    }
+
+    
+}
