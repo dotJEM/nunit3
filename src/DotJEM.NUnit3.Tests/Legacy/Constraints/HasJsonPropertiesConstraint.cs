500593ef4ba135ecbfac2bf171e578087559c484
Jens Melgaard <admin@it-links.dk>
2023-03-02 14:28:13 +0100

    another

--- src/DotJEM.NUnit3.Tests/Legacy/Constraints/HasJsonPropertiesConstraint.cs
+++ src/DotJEM.NUnit3.Tests/Legacy/Constraints/HasJsonPropertiesConstraint.cs
@@ -96,5 +96,59 @@ namespace DotJEM.NUnit3.Tests.Legacy.Constraints
                                                       "\r\n  Actual object did not contain a property named 'arr[1].arr[1].empty'" +
                                                       "\r\n"));
         }
+
+        [Test]
+          public void ApplyTo_DeepObjects2_Fails()
+        {
+            JObject expected = JObject.Parse("{" +
+                                             "  val: {" +
+                                             "    A: { " +
+                                             "      name: 'fox-01'," +
+                                             "      arr: [" +
+                                             "        { type: 'X01', number: 42 }," +
+                                             "        { type: 'Y02', empty: [] }" +
+                                             "      ]" +
+                                             "    }," +
+                                             "    B: { " +
+                                             "      name: 'fox-02'," +
+                                             "      arr: [" +
+                                             "        { type: 'X01', number: 42 }," +
+                                             "        { type: 'Y02', empty: [] }" +
+                                             "      ]" +
+                                             "    }" +
+                                             "  }" +
+                                             "}");
+            JObject actual = JObject.Parse("{" +
+                                           "  val: {" +
+                                           "    A: { " +
+                                           "      name: 'fox-01'," +
+                                           "      arr: [" +
+                                           "        { type: 'X01' }," +
+                                           "        { type: 'Y02' }" +
+                                           "      ]" +
+                                           "    }," +
+                                           "    B: { " +
+                                           "      name: 'fox-02'," +
+                                           "      arr: [" +
+                                           "        { type: 'X01' }," +
+                                           "        { type: 'Y02' }" +
+                                           "      ]" +
+                                           "    }" +
+                                           "  }" +
+                                           "}");
+
+            HasJsonPropertiesConstraint constraint = new HasJsonPropertiesConstraint(expected);
+            ConstraintResult result = constraint.ApplyTo(actual);
+            Assert.That(result, Has.Property<ConstraintResult>(r => r.IsSuccess).False, result.ToString());
+
+            string str = result.ToString();
+
+            Assert.That(result.ToString(), Is.EqualTo("  DotJEM.NUnit3.Legacy.Constraints.HasJsonPropertiesConstraint Failed!" +
+                                                      "\r\n  Actual object did not contain a property named 'val.A.arr[0].number'" +
+                                                      "\r\n  Actual object did not contain a property named 'val.A.arr[1].empty'" +
+                                                      "\r\n  Actual object did not contain a property named 'val.B.arr[0].number'" +
+                                                      "\r\n  Actual object did not contain a property named 'val.B.arr[1].empty'" +
+                                                      "\r\n"));
+        }
     }
 }
