9cd18200bef4a03bef62fda1961a81fcadaf33f3
jmd <jmd@Systematic.com>
2024-01-17 15:00:47 +0100

    fix test

--- src/DotJEM.NUnit3.Tests/Constraints/Collections/EnumerableEqualsConstraintTest.cs
+++ src/DotJEM.NUnit3.Tests/Constraints/Collections/EnumerableEqualsConstraintTest.cs
@@ -21,7 +21,7 @@ namespace DotJEM.NUnit3.Tests.Constraints.Collections
             ConstraintResult result = constraint.ApplyTo(new[] { 1, 2, 2, 4, 5 });
 
             Assert.That(result, Has.Property<ConstraintResult>(r => r.IsSuccess).False, result.ToString());
-            Assert.That(result.ToString(), Is.EqualTo("  Expected: element at [2] to be: 3\r\n  But was:  2\r\n"));
+            Assert.That(result.ToString(), Is.EqualTo("Collections did not match.\r\n  Element at [2]:\r\n    Expected: 3\r\n    But was:  2\r\n  \r\n"));
         }
 
         [Test, Explicit("This test-case is meant to fail to verify how the error message is displayed in NUnit runners, e.g. ReSharpers runner")]
